e7ae742ca71136ed856c2e2dd60ff52e2651b1c0
krishnasagar <sagar474@hotmail.com>
2025-05-23 13:24:14 +0100

    add container width in tailwind config

--- sites/store1/storefront/tailwind.config.cjs
+++ sites/store1/storefront/tailwind.config.cjs
@@ -8,6 +8,17 @@ module.exports = {
 				primary: colors.blue,
 			},
 		},
+		container: {
+			center: true,
+			padding: '1rem',
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+				'2xl': '1400px', // you can define custom breakpoints
+			},
+		},
 		variants: {
 			extend: {
 				opacity: ['disabled'],
