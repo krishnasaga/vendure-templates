00f34abf5d55727214ee81c5054177a230b86cc7
krishnasagar <sagar474@hotmail.com>
2025-05-28 10:48:09 +0100

    add cors config to store

--- sites/store1/store/src/vendure-config.ts
+++ sites/store1/store/src/vendure-config.ts
@@ -1,9 +1,9 @@
 import {
-    dummyPaymentHandler,
-    DefaultJobQueuePlugin,
-    DefaultSchedulerPlugin,
-    DefaultSearchPlugin,
-    VendureConfig,
+  dummyPaymentHandler,
+  DefaultJobQueuePlugin,
+  DefaultSchedulerPlugin,
+  DefaultSearchPlugin,
+  VendureConfig,
 } from "@vendure/core";
 import {
   defaultEmailHandlers,
@@ -13,97 +13,93 @@ import {
 import { AssetServerPlugin } from "@vendure/asset-server-plugin";
 import { AdminUiPlugin } from "@vendure/admin-ui-plugin";
 import { GraphiqlPlugin } from "@vendure/graphiql-plugin";
-import { razorpayPaymentHandler } from './plugins/razorpay-payment-handler';
+import { razorpayPaymentHandler } from "./plugins/razorpay-payment-handler";
 import "dotenv/config";
 import path from "path";
+import cors from "cors";
 
 const IS_DEV = process.env.APP_ENV === "dev";
 const serverPort = +process.env.PORT || 3000;
 
 export const config: VendureConfig = {
-    apiOptions: {
-        port: serverPort,
+  apiOptions: {
+    port: serverPort,
     adminApiPath: "admin-api",
     shopApiPath: "shop-api",
-        // The following options are useful in development mode,
-        // but are best turned off for production for security
-        // reasons.
     ...(IS_DEV
       ? {
-            adminApiDebug: true,
-            shopApiDebug: true,
+          adminApiDebug: true,
+          shopApiDebug: true,
         }
       : {}),
-    },
-    authOptions: {
+    middleware: [
+      {
+        route: "assets",
+        handler: cors({
+          origin: [
+            "http://localhost:8080",
+            "https://singular-salamander-093b82.netlify.app",
+          ],
+        }),
+      },
+    ],
+  },
+  authOptions: {
     tokenMethod: ["bearer", "cookie"],
-        superadminCredentials: {
-            identifier: process.env.SUPERADMIN_USERNAME,
-            password: process.env.SUPERADMIN_PASSWORD,
-        },
-        cookieOptions: {
-          secret: process.env.COOKIE_SECRET,
-        },
+    superadminCredentials: {
+      identifier: process.env.SUPERADMIN_USERNAME,
+      password: process.env.SUPERADMIN_PASSWORD,
     },
-    dbConnectionOptions: {
+    cookieOptions: {
+      secret: process.env.COOKIE_SECRET,
+    },
+  },
+  dbConnectionOptions: {
     type: "better-sqlite3",
-        // See the README.md "Migrations" section for an explanation of
-        // the `synchronize` and `migrations` options.
-        synchronize: false,
+    synchronize: false,
     migrations: [path.join(__dirname, "./migrations/*.+(js|ts)")],
-        logging: false,
+    logging: false,
     database: path.join(__dirname, "../vendure.sqlite"),
-    },
-    paymentOptions: {
-        paymentMethodHandlers: [
-          razorpayPaymentHandler,
-        ],
-    },
-    // When adding or altering custom field definitions, the database will
-    // need to be updated. See the "Migrations" section in README.md.
-    customFields: {},
-    plugins: [
-        GraphiqlPlugin.init(),
-        AssetServerPlugin.init({
+  },
+  paymentOptions: {
+    paymentMethodHandlers: [razorpayPaymentHandler],
+  },
+  customFields: {},
+  plugins: [
+    GraphiqlPlugin.init(),
+    AssetServerPlugin.init({
       route: "assets",
       assetUploadDir: path.join(__dirname, "../static/assets"),
-            // For local dev, the correct value for assetUrlPrefix should
-            // be guessed correctly, but for production it will usually need
-            // to be set manually to match your production url.
-      assetUrlPrefix: IS_DEV
-        ? undefined
-        : `${process.env.ROOT_UR}/assets/`,
-        }),
-        DefaultSchedulerPlugin.init(),
-        DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
-        DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
-        EmailPlugin.init({
-            devMode: true,
+      assetUrlPrefix: IS_DEV ? undefined : `${process.env.ROOT_UR}/assets/`,
+    }),
+    DefaultSchedulerPlugin.init(),
+    DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
+    DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
+    EmailPlugin.init({
+      devMode: true,
       outputPath: path.join(__dirname, "../static/email/test-emails"),
       route: "mailbox",
-            handlers: defaultEmailHandlers,
+      handlers: defaultEmailHandlers,
       templateLoader: new FileBasedTemplateLoader(
         path.join(__dirname, "../static/email/templates")
       ),
-            globalTemplateVars: {
-                // The following variables will change depending on your storefront implementation.
-                // Here we are assuming a storefront running at http://localhost:8080.
-                fromAddress: '"example" <noreply@example.com>',
+      globalTemplateVars: {
+        fromAddress: '"example" <noreply@example.com>',
         verifyEmailAddressUrl: "http://localhost:8080/verify",
         passwordResetUrl: "http://localhost:8080/password-reset",
         changeEmailAddressUrl:
           "http://localhost:8080/verify-email-address-change",
-            },
-        }),
-        AdminUiPlugin.init({
+      },
+    }),
+    AdminUiPlugin.init({
       route: "admin",
-            port: serverPort + 2,
-            adminUiConfig: {
+      port: serverPort + 2,
+      adminUiConfig: {
         apiHost: process.env.ROOT_URL,
-        apiPort: 443,
+        apiPort: +process.env.PORT,
         adminApiPath: "admin-api",
         tokenMethod: "bearer",
-            },
-        }),
-    ],
+      },
+    }),
+  ],
 };
