fc7a9281c672d798a4b6c1da63fa5a024b2acac0
krishnasagar <sagar474@hotmail.com>
2025-07-04 16:22:30 +0100

    add usps VariantA component

---
+++ package/react-cart/src/components/USPs-VariantA/index.tsx
@@ -0,0 +1,48 @@
+const values = [
+  {
+    image: "https://d2gansr34f2te0.cloudfront.net/usp1.avif   ",
+    title: "Femme Forward",
+  },
+  {
+    image: "https://d2gansr34f2te0.cloudfront.net/Circular.webp   ",
+    title: "Fashion that Returns",
+  },
+  {
+    image: "https://d2gansr34f2te0.cloudfront.net/Sustainable.avif   ",
+    title: "Earth Kind",
+  },
+  {
+    image: "https://d2gansr34f2te0.cloudfront.net/Heirloom%20Crafts.avif   ",
+    title: "Timeless Traditions",
+  },
+  {
+    image: "https://d2gansr34f2te0.cloudfront.net/Easy%20Returns.avif   ",
+    title: "Hassle-Free Returns",
+  },
+];
+
+const ValuesSection = ({ className }: { className: string }) => {
+  return (
+    <section className={`container mx-auto flex justify-center gap-4 md:gap-14 ${className}`}>
+      {values.map((value, index) => (
+        <div className=" md:h-[100px] w-auto flex flex-col items-center justify-between">
+          <img
+            key={index}
+            src={value.image}
+            alt={value.title}
+            className="w-14 h-14 md:w-16 md:h-16 md:w-20 md:h-20 object-contain mb-2"
+          />
+          <h4
+            className={
+              "text-[10px] md:text-sm font-medium leading-tight text-center"
+            }
+          >
+            {value.title}
+          </h4>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default ValuesSection;
