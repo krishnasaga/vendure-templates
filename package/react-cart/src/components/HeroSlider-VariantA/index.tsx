e09154aa7d2dbdbf3e2d103285e7e9788bce69d4
krishnasagar <sagar474@hotmail.com>
2025-07-06 12:31:08 +0100

    add hero slider A updates

--- package/react-cart/src/components/HeroSlider-VariantA/index.tsx
+++ package/react-cart/src/components/HeroSlider-VariantA/index.tsx
@@ -1,17 +1,16 @@
-import { useEffect, useState } from 'react';
-import slide1 from './slide1.png';
-import slide3 from './slide3.png';
-import slide4 from './slide4.png';
-import slide5 from './slide5.png';
+import { useEffect, useRef, useState } from "react";
 
-const SlideImage = ({ src = "", alt = '', className = '', ...props }) => {
+const SlideImage = ({ src = "", alt = "", className = "", ...props }) => {
   const [loaded, setLoaded] = useState(false);
+  const imageRef = useRef<HTMLImageElement>(null);
 
   useEffect(() => {
     if (!src) return;
-    const img = new window.Image();
-    img.src = src;
-    img.onload = () => setLoaded(true);
+    if (!imageRef?.current) {
+      imageRef.current = new window.Image();
+    }
+    imageRef.current.src = src;
+    imageRef.current.onload = () => setLoaded(true);
   }, [src]);
 
   return (
@@ -19,8 +18,8 @@ const SlideImage = ({ src = "", alt = '', className = '', ...props }) => {
       src={src}
       alt={alt}
       object-fit={"contain"}
-      className={`${className} transition-opacity duration-700 ${loaded ? 'opacity-100' : 'opacity-0'}`}
-      style={{ pointerEvents: loaded ? undefined : 'none' }}
+      className={`${className} transition-opacity duration-700 ${loaded ? "opacity-100" : "opacity-0"}`}
+      style={{ pointerEvents: loaded ? undefined : "none" }}
       draggable={false}
       {...props}
     />
@@ -28,88 +27,132 @@ const SlideImage = ({ src = "", alt = '', className = '', ...props }) => {
 };
 
 const Slide1 = () => (
-  <div className="relative bg-[#f7e7e7] bg-[url('https://d2gansr34f2te0.cloudfront.net/gray-floral.png')] w-full overflow-hidden md:h-full flex flex-col md:flex-row items-center justify-center py-4 md:py-8 ">
+  <div className="relative bg-primary-100 bg-[url('https://d2gansr34f2te0.cloudfront.net/gray-floral.png')] w-full overflow-hidden md:h-full flex flex-col md:flex-row items-center justify-center py-4 md:py-8 ">
     <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 md:px-8 relative z-10">
       <div className="flex flex-col items-center md:items-start text-center md:text-left py-4 md:py-8 z-10">
-        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">Khai Fashion</p>
-        <h1 className="text-5xl md:text-7xl font-extrabold text-neutral-dark leading-tight mb-4 animate-slide-left-fade">
+        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">
+          PASTEL WAVE
+        </p>
+        <h1 className="text-5xl md:text-7xl font-extrabold text-neautral-700 leading-tight mb-4 animate-slide-left-fade">
           Flat 25% OFF on Summer Styles!
         </h1>
-        <p className="text-xl md:text-2xl text-neutral-dark mb-6 animate-slide-left-fade">
+        <p className="text-xl md:text-2xl text-neautral-700 mb-6 animate-slide-left-fade">
           Flowy fits, bold prints — perfect for warm days.
         </p>
-        <a href="/collections/summer-fashion/" className="delay-<number> animate-slide-left-fade px-5 py-4 md:text-2xl bg-primary-500 text-primary-100 font-bold rounded-md shadow-md hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105">
+        <a
+          href="/collections/summer-fashion/"
+          className="animate-slide-up-fade px-5 py-4 md:text-2xl bg-primary-500 text-white font-bold hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105"
+        >
           Explore Collection
         </a>
       </div>
       <div className="flex-shrink-0 mt-8 md:mt-0 md:ml-auto md:w-1/2 w-full flex justify-center items-center overflow-hidden ">
-        <SlideImage src={slide1} alt="Slide 1" width={500} height={400} className="animate-slide-right-fade" />
+        <SlideImage
+          src={"https://d2gansr34f2te0.cloudfront.net/hero-2.png"}
+          alt="Slide 1"
+          width={500}
+          height={400}
+          className="animate-slide-right-fade"
+        />
       </div>
     </div>
   </div>
 );
 
 const Slide2 = () => (
-  <div className="relative w-full overflow-hidden md:h-full flex flex-col md:flex-row items-center justify-center py-4 md:py-0 bg-[#e7eafc] bg-[url('https://d2gansr34f2te0.cloudfront.net/gray-floral.png')] bg-center bg-repeat">
+  <div className="relative w-full overflow-hidden md:h-full flex flex-col md:flex-row items-center justify-center py-4 md:py-0 bg-primary-100 bg-[url('https://d2gansr34f2te0.cloudfront.net/gray-floral.png')] bg-center bg-repeat">
     <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 md:px-8 relative z-10 h-full mt-0">
       <div className="flex flex-col items-center md:items-start text-center md:text-left py-4 md:py-8 z-10">
-        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">Khai Fashion</p>
-        <h1 className="text-5xl md:text-7xl font-extrabold text-neutral-dark leading-tight mb-4 animate-slide-left-fade">
+        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">
+          PASTEL WAVE
+        </p>
+        <h1 className="text-5xl md:text-7xl font-extrabold text-neautral-700 leading-tight mb-4 animate-slide-left-fade">
           What defines effortless style?
         </h1>
-        <p className="text-xl md:text-2xl text-neutral-dark mb-2 animate-slide-left-fade">
+        <p className="text-xl md:text-2xl text-neautral-700 mb-2 animate-slide-left-fade">
           The right drape, the right detail.
         </p>
-        <a href="/collections/summer-fashion/" className="animate-slide-left-fade px-5 py-4 md:text-2xl bg-primary-500 text-primary-100 font-bold rounded-md shadow-md hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105">
+        <a
+          href="/collections/summer-fashion/"
+          className="animate-slide-up-fade px-5 py-4 md:text-2xl bg-primary-500 text-white font-bold hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105 animate-ping"
+        >
           Explore Collection
         </a>
       </div>
-      <div className="md:mt-0 md:ml-auto md:w-1/2 w-full h-full flex flex-col justify-start items-center ">
-        <SlideImage src={'https://d2gansr34f2te0.cloudfront.net/hero-2.png'} alt="Slide 2 top" width={500} height={800} className="animate-slide-right-fade" />
+      <div className="flex-shrink-0 mt-8 md:mt-0 md:ml-auto md:w-1/2 w-full flex justify-center items-center overflow-hidden ">
+        <SlideImage
+          src={"https://d2gansr34f2te0.cloudfront.net/hero-2.png"}
+          alt="Slide 1"
+          width={500}
+          height={400}
+          className="animate-slide-right-fade"
+        />
       </div>
     </div>
   </div>
 );
 
 const Slide3 = () => (
-  <div className="relative w-full overflow-hidden md:h-full flex flex-col md:flex-row items-center justify-center py-4 md:py-8 bg-repeat bg-tile-x bg-[#f7e7f7] bg-pastel-pink bg-[url('https://d2gansr34f2te0.cloudfront.net/gray-floral.png')]">
+  <div className="relative w-full overflow-hidden md:h-full flex flex-col md:flex-row items-center justify-center py-4 md:py-8 bg-repeat bg-tile-x bg-[#f7e7f7] bg-primary-100 bg-[url('https://d2gansr34f2te0.cloudfront.net/gray-floral.png')]">
     <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 md:px-8 relative z-10">
       <div className="flex flex-col items-center md:items-start text-center md:text-left py-4 md:py-8 z-10">
-        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">Khai Fashion</p>
-        <h1 className="text-5xl md:text-7xl font-extrabold text-neutral-dark leading-tight mb-4 animate-slide-left-fade">
+        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">
+          PASTEL WAVE
+        </p>
+        <h1 className="text-5xl md:text-7xl font-extrabold text-neautral-900  leading-tight mb-4 animate-slide-left-fade">
           Style Meets Substance!
         </h1>
-        <p className="text-xl md:text-2xl text-neutral-dark mb-2 animate-slide-left-fade">
+        <p className="text-xl md:text-2xl text-neautral-700 mb-2 animate-slide-left-fade">
           From everyday elegance to festive glam — we've got you covered.
         </p>
-        <a href="/collections/summer-fashion/" className="animate-slide-left-fade px-5 py-4 md:text-2xl bg-primary-500 text-primary-100 font-bold rounded-md shadow-md hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105">
+        <a
+          href="/collections/summer-fashion/"
+          className="animate-slide-up-fade px-5 py-4 md:text-2xl bg-primary-500 text-white font-bold hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105"
+        >
           Explore Collection
         </a>
       </div>
-      <div className="flex-shrink-0 mt-8 md:mt-0 md:ml-auto md:w-1/2 w-full flex justify-center items-center overflow-hidden">
-        <SlideImage src={slide3} alt="Slide 3" width={500} height={600} className="animate-slide-right-fade" />
+      <div className="flex-shrink-0 mt-8 md:mt-0 md:ml-auto md:w-1/2 w-full flex justify-center items-center overflow-hidden ">
+        <SlideImage
+          src={"https://d2gansr34f2te0.cloudfront.net/hero-2.png"}
+          alt="Slide 1"
+          width={500}
+          height={400}
+          className="animate-slide-right-fade"
+        />
       </div>
     </div>
   </div>
 );
 
 const Slide4 = () => (
-  <div className="relative w-full overflow-hidden md:h-full flex flex-col md:flex-row items-center justify-center py-4 md:py-8 bg-[#9f7657] bg-[url('https://d2gansr34f2te0.cloudfront.net/gray-floral.png')]">
+  <div className="relative w-full overflow-hidden md:h-full flex flex-col md:flex-row items-center justify-center py-4 md:py-8 bg-primary-100 bg-[url('https://d2gansr34f2te0.cloudfront.net/gray-floral.png')]">
     <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 md:px-8 relative z-10">
       <div className="flex flex-col items-center md:items-start text-center md:text-left py-4 md:py-8 z-10">
-        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">Khai Fashion</p>
-        <h1 className="text-5xl md:text-7xl font-extrabold text-neutral-dark leading-tight mb-4 animate-slide-left-fade">
+        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">
+          PASTEL WAVE
+        </p>
+        <h1 className="text-5xl md:text-7xl font-extrabold text-neautral-900  leading-tight mb-4 animate-slide-left-fade">
           Festive Combo Pack!
         </h1>
-        <p className="text-xl md:text-2xl text-neutral-dark mb-2 animate-slide-right-fade">
+        <p className="text-xl md:text-2xl text-neautral-900  mb-2 animate-slide-right-fade">
           Get handcrafted dupattas with our bestselling tunics.
         </p>
-        <a href="/collections/summer-fashion/" className="animate-slide-right-fade px-5 py-4 md:text-2xl bg-primary-500 text-primary-100 font-bold rounded-md shadow-md hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105">
+        <a
+          href="/collections/summer-fashion/"
+          className="animate-slide-up-fade px-5 py-4 md:text-2xl text-neautral-900  text-white font-bold hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105"
+        >
           Explore Collection
         </a>
       </div>
-      <div className="flex-shrink-0 mt-8 md:mt-0 md:ml-auto md:w-1/2 w-full flex justify-center items-center overflow-hidden">
-        <SlideImage src={slide4} alt="Slide 4" width={500} height={600} className="animate-slide-right-fade" />
+      <div className="flex-shrink-0 mt-8 md:mt-0 md:ml-auto md:w-1/2 w-full flex justify-center items-center overflow-hidden ">
+        <SlideImage
+          src={"https://d2gansr34f2te0.cloudfront.net/hero-2.png"}
+          alt="Slide 1"
+          width={500}
+          height={400}
+          className="animate-slide-right-fade"
+        />
       </div>
     </div>
   </div>
@@ -119,31 +162,42 @@ const Slide5 = () => (
   <div className="relative w-full overflow-hidden md:h-full flex flex-col md:flex-row items-center justify-center py-4 md:py-8 bg-primary-100 bg-[url('https://d2gansr34f2te0.cloudfront.net/gray-floral.png')]">
     <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 md:px-8 relative z-10">
       <div className="flex flex-col items-center md:items-start text-center md:text-left py-4 md:py-8 z-10">
-        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">Khai Fashion</p>
-        <h1 className="text-5xl md:text-7xl font-extrabold text-neutral-dark leading-tight mb-4 animate-slide-right-fade">
+        <p className="text-sm md:text-base text-primary-500 mb-2 uppercase tracking-wide">
+          PASTEL WAVE
+        </p>
+        <h1 className="text-5xl md:text-7xl font-extrabold text-neautral-900  leading-tight mb-4 animate-slide-right-fade">
           Handmade Accessories
         </h1>
-        <p className="text-xl md:text-2xl font-bold text-green-500 mb-6 animate-slide-left-fade">
+        <p className="text-xl md:text-2xl font-bold text-neautral-900  mb-6 animate-slide-left-fade">
           Made to complete your look
         </p>
-        <a href="/collections/summer-fashion/" className="animate-slide-left-fade px-5 py-4 md:text-2xl bg-primary-500 text-primary-100 font-bold rounded-md shadow-md hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105">
+        <a
+          href="/collections/summer-fashion/"
+          className="animate-slide-up-fade px-5 py-4 md:text-2xl bg-primary-500 text-white font-bold hover:bg-secondary-500 transition duration-300 ease-in-out transform hover:scale-105"
+        >
           Explore Collection
         </a>
       </div>
-      <div className="flex-shrink-0 mt-8 md:mt-0 md:ml-auto md:w-1/2 w-full flex justify-center items-center overflow-hidden">
-        <SlideImage src={slide5} alt="Slide 5" width={500} height={600} className="animate-slide-right-fade" />
+      <div className="flex-shrink-0 mt-8 md:mt-0 md:ml-auto md:w-1/2 w-full flex justify-center items-center overflow-hidden ">
+        <SlideImage
+          src={"https://d2gansr34f2te0.cloudfront.net/hero-2.png"}
+          alt="Slide 1"
+          width={500}
+          height={400}
+          className="animate-slide-right-fade"
+        />
       </div>
     </div>
   </div>
 );
 
-export const HeroCarousel = (props:{className: string}) => {
+export const HeroCarousel = (props: { className: string }) => {
   const slides = [<Slide1 />, <Slide2 />, <Slide3 />, <Slide4 />, <Slide5 />];
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex(prev => (prev + 1) % slides.length);
+      setIndex((prev) => (prev + 1) % slides.length);
     }, 5000);
     return () => clearInterval(interval);
   }, [slides.length]);
@@ -158,7 +212,7 @@ export const HeroCarousel = (props:{className: string}) => {
           <button
             key={i}
             onClick={() => setIndex(i)}
-            className={`w-3 h-3 rounded-full transition-colors duration-300 ${i === index ? 'bg-white' : 'bg-gray-400'}`}
+            className={`w-3 h-3 rounded-full transition-colors duration-300 ${i === index ? "bg-white" : "bg-gray-400"}`}
           />
         ))}
       </div>
