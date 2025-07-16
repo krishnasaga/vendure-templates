40efe66f37e2804b85c726006d7ddfe5e6431e71
krishnasagar <sagar474@hotmail.com>
2025-07-16 15:07:30 +0100

    add redirects for routes

--- package/storefront-pastelweave/src/App.tsx
+++ package/storefront-pastelweave/src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import CollectionPage from './Pages/Collections'; // This will handle /collections/:slug
 
 function App() {
-
   return (
-    <>
-      <Home/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collections/:slug" element={<CollectionPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
